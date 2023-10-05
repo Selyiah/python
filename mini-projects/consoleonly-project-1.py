6f2961b47d962260a059019d45e47a77ae876c5d
Sely <140277594+Selyiah@users.noreply.github.com>
2023-10-05 17:57:18 +0100

    Create consoleonly-project-1.py

---
+++ mini-projects/consoleonly-project-1.py
@@ -0,0 +1,18 @@
+chairs = 15
+nails = 4
+total_nails = chairs * nails
+message = 'I need to buy {} nails'.format(total_nails)
+print(message)
+
+my_name = "Penelope"
+my_age = 29
+message = 'My name is {} and I am {} years old'.format(my_name, my_age)
+print(message)
+
+total_boxes = 6
+eggs_per_box = 6
+eggs_per_omelette = 4
+total_eggs = eggs_per_box * total_boxes
+total_omelettes = total_eggs // eggs_per_omelette
+message = 'You can make {} omelettes with {} boxes of eggs'.format(total_omelettes, total_boxes)
+print(message)
