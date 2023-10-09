24936bf4206f7beb8b13009e260d7997d31f83eb
Sely <140277594+Selyiah@users.noreply.github.com>
2023-10-09 20:46:35 +0100

    Create inputs&outputs.py

---
+++ inputs&outputs.py
@@ -0,0 +1,33 @@
+# outputs
+cats = 10
+cans = 2
+total_cans = cats + cans
+
+output = str(cats) + " cats eat " + str(total_cans) + " cans"
+print(output)
+
+animal = "cat"
+description = "silly"
+output = "{} {}".format(animal, description)
+print(output)
+
+# this is how to make a comment that doesn't affect the code.
+
+kids = input("How many kids in the class do you have?")
+total_kids = int(kids) + 25
+print(total_kids)
+
+# input means you have to respond the answer in the running window (console)
+
+friends = input('How many friends? ')
+pizzas = int(friends) * 0.5
+print('You need {} pizzas for {} friends'.format(pizzas, friends))
+
+
+# Examples of python modules:
+# math --> mathematical functions
+# datetime --> date and time value manipulation
+# timeit --> time the execution of small blocks of Python code
+# re --> regular expressions (pattern search)
+# copy --> duplicating objects
+
