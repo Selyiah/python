aee3f8a4f46e9f7a3197df467005f7756bce6221
Sely <140277594+Selyiah@users.noreply.github.com>
2023-10-09 21:14:37 +0100

    Update loops.py

--- loops.py
+++ loops.py
@@ -52,3 +52,17 @@ while store_capacity > 0:
     store_capacity -= 1
 print("\nPlease wait for someone to exit the store.")
 
+# Rewrite this code to use a for loop and the range() function:
+print('~' * 0)
+print('~' * 1)
+print('~' * 2)
+print('~' * 3)
+print('~' * 4)
+print('~' * 5)
+print('~' * 6)
+print('~' * 7)
+print('~' * 8)
+
+for number in range(9):
+    print('~' * number)
+
