9ec779e026428f0f5b77b47c14ae5a249e4187b7
Sely <140277594+Selyiah@users.noreply.github.com>
2023-10-28 16:44:28 +0100

    Create PIP&txt-files.py

---
+++ mini-projects/PIP&txt-files.py
@@ -0,0 +1,19 @@
+# Q1
+# PIP stands for "Preferred Installer Program" and is used as a recommended tool to install additional libraries and
+# packages that are not part of the standard Python library.
+
+# benefits:
+# - The latest version available can be installed but also target alternative versions of choice.
+# - Can create a file text with a custom requirements package
+
+# Q2
+with open('poem.txt', 'w+') as poem_file:  # change the 'r' to 'w+' - from reading to writing
+    poem = 'I like Python and I am not very good at poems'  # moved this line below for personal pref
+    poem_file.write(poem)
+
+poem = 'I like Python and I am not very good at poems'
+with open('poem.txt', 'w+') as poem_file:   # change the 'r' to 'w+' - from reading to writing
+    poem_file.write(poem)
+
+# Q3
+# get_pokemon.py
